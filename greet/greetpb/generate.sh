61172dd20d1cb02255e5078a01465ff3f25f5113
peter <peteryocum1@gmail.com>
2022-03-17 19:37:01 -0400

    create python protobuf files, create small example client for language interoperability

--- greet/greetpb/generate.sh
+++ greet/greetpb/generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 protoc greet/greetpb/greet.proto --go_out=. --go-grpc_out=.
+python3 -m grpc_tools.protoc -I./greet/greetpb --python_out=./greet/greet_client --grpc_python_out=./greet/greet_client ./greet/greetpb/greet.proto 
+#protoc greet/greetpb/greet.proto --python_out=../greet_client --grpc_python_out=../greet_client
